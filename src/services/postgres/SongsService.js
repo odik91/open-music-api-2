201d53777c10d12bde84c811afb6bb9face290e2
odik91 <ali.shoddiqien@gmail.com>
2021-07-20 14:30:57 +0700

    adding addsong method in postgres-SongsService.js

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -1,9 +1,37 @@
 /* eslint-disable no-underscore-dangle */
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+// const NotFoundError = require('../../exceptions/NotFoundError');
 
 class SongsSevice {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addSong({
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
 
